28f513ee043462d483659fd6d9eef0ec9f0690c8
Jorge-MoralesV <jorgemoralesv99@gmail.com>
2024-10-14 14:31:27 -0600

    angular

--- assets/js/funcion.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("icon-menu").addEventListener("click", mostrar_menu);
-
-function mostrar_menu() {
-    document.getElementById("show-menu").classList.toggle('show-lateral');
-}
